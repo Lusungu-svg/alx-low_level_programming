d2dce4887c37d79924c3ebcd3a215ca0714bf218
Lusungu-svg <mwalelisa255@gmail.com>
2023-03-16 12:01:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /*
- *4. Hello, puts
+ *Hello, puts
  */
 
-int main (void)
+int main(void)
+{           
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
-	put("\"Programming is like building a multilingual puzzle");
-	return (0)
+}
