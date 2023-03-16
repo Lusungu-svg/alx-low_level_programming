a16f812330e586b47561edc0dc67921fab32b38f
Lusungu-svg <mwalelisa255@gmail.com>
2023-03-16 13:24:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,12 @@
 /**
  * main - print code using printf
  *
- * Return: (0)
+ * Return: 0
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
+
 }
