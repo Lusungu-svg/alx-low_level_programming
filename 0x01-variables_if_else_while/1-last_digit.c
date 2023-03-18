c8f59bf8da3d6637b9110284684863e847c2bc2e
Lusungu-svg <mwalelisa255@gmail.com>
2023-03-18 12:33:15 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,11 +19,12 @@ int main(void)
 	{
 		printf("and is greater than 5");
 	}
-	if else(x == 0)
+	if (x == 0)
 		printf("and is 0");
 	{
-	else
+	if (x < 6 && x != 0)
 		printf("and is less than 6 and not 0");
 	}
+	printf("\n")
 	return (0);
 }
